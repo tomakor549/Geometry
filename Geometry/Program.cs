eb2782a74c5d25c6db7debd39e5f50dd8e6db0ee
Tenebra <venetor7@gmail.com>
2022-11-29 23:22:26 +0100

    Modify: Merge

--- Geometry/Program.cs
+++ Geometry/Program.cs
@@ -1,8 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Geometry.Models;
-using System.Drawing;
-using System.Numerics;
-
-Console.WriteLine("Hello, World!");
-var a = new Vector<double, double, double>();
-Point3D
+﻿Console.WriteLine("Hello, World!");
